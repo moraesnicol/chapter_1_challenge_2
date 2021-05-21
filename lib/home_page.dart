6c9756e8fb04f2e603882611bb5925b55706b13e
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-21 04:18:07 -0300

    init

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,7 +5,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: Text("Notes"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Card(
+          child: ListTile(
+            title: Text("minha pri"),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("object");
+        },
       ),
     );
   }
